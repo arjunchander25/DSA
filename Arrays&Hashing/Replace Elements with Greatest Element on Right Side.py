1c9bc9261d4865c88d1d7b493555191fe1528704
arjunchander25 <143799534+arjunchander25@users.noreply.github.com>
2023-09-01 19:10:15 +0530

    Create Replace Elements with Greatest Element on Right Side.py

---
+++ Arrays&Hashing/Replace Elements with Greatest Element on Right Side.py
@@ -0,0 +1,11 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        i=len(arr)-2
+        maxx=0
+        while i>=0:
+            maxx=max(arr[i+1],maxx)
+            arr[i+1]=maxx
+            i=i-1
+        arr.append(-1)
+        return arr[1:]
+        
